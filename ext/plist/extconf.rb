171477a46d796cea62d287f255dde82bd6c46b6c
Kevin Ballard <kevin@sb.org>
2009-09-17 21:47:20 -0700

    Delete architecture/SDK flags from extconf.rb
    These flags were preventing compilation on Snow Leopard. They were only included
    because this project was built for use in TextMate, but they don't need to be
    left in the general-purpose installation.

--- ext/plist/extconf.rb
+++ ext/plist/extconf.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/ruby
 require 'mkmf'
-newFlags = " -arch ppc -arch i386 -isysroot /Developer/SDKs/MacOSX10.4u.sdk"
-$CFLAGS += newFlags
-$LDFLAGS += ' -framework CoreFoundation' + newFlags + ' -undefined suppress -flat_namespace'
+$LDFLAGS += ' -framework CoreFoundation -undefined suppress -flat_namespace'
 $LIBRUBYARG_SHARED=""
 create_makefile("osx/plist")
